a90ee3d4664ecd0e26e5f225e48923d366346f1c
Melissa Holt <melissa.holt21@gmail.com>
2019-11-12 00:16:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line(array)
 array = []
   if array.length >= 1
     array.each_with_index do |new_person, index|
-    deli_line << "#{index+1}. #{person} "
+    deli_line << "#{index+1}. #{person}"
     end
-    return array.join  
+    return array.join(" ") 
   else puts "The line is currently empty."
   end
 end
