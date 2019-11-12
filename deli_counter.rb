8e889ea1ad6b67c8f8b9f01a958827ec8df785b7
Melissa Holt <melissa.holt21@gmail.com>
2019-11-12 01:00:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ deli = ["The line is currently:"]
 end
 
 def take_a_number(katz_deli, name)
-      katz_deli << 
+      katz_deli << (name)
       puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
