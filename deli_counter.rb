9455de258cdde3ec9d49b5b126a22d1623dde56a
Melissa Holt <melissa.holt21@gmail.com>
2019-11-12 00:24:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,1 @@
-katz_deli = []
 
-def line(array)
-deli = ["The line is currently:"]
-  if array.length >= 1
-    array.each_with_index do |new_person, index|
-    deli << "#{index+1}. #{new_person}"
-    end
-    return deli.join(" ") 
-  else puts "The line is currently empty."
-  end
-end
