a8288f98f2721a74f97d5c7d522d0407acea2712
Melissa Holt <melissa.holt21@gmail.com>
2019-11-12 00:41:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,6 +11,6 @@ end
 
 
 def take_a_number(array,string)
-    array.each_with_index do |n, index| room_assignments << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    array.each_with_index do |, index| room_assignments << "Hello, #{name}! You'll be assigned to room #{index+1}!"
   puts "Welcome, _. You are number _ in line"
 end
