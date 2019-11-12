9197348643ac6de1037454fa18e3e14f7ed6dda4
Melissa Holt <melissa.holt21@gmail.com>
2019-11-12 01:16:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,5 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  array.each do |next_person| ba << "Hello, my name is #{speaker}."
-    end
-  puts "Currently serving #{}."
+  array.each do |next_person| puts "Currently serving #{next_person}."
 end
